8a09ba7d88412c985b6f9a9d0e5775191fa25ef6
Timothy Redaelli <timothy@redaelli.eu>
2011-08-11 22:44:28 +0200

    NG-0.7.4 version ready!

--- include/ec_version.h
+++ include/ec_version.h
@@ -3,10 +3,10 @@
 #ifndef EC_VERS_H
 #define EC_VERS_H
 
-#define EC_VERSION            "NG-0.7.4pre"
+#define EC_VERSION            "NG-0.7.4"
 #define EC_VERSION_MAJOR      0
 #define EC_VERSION_MINOR      7
-#define EC_VERSION_REVISION   3
+#define EC_VERSION_REVISION   4
 #define EC_PROGRAM            "ettercap"
 #define EC_COPYRIGHT          "2001-2005"
 #define EC_AUTHORS            "ALoR & NaGA"
